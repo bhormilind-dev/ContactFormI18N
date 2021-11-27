f863b2fc5b44540e96ac86c558315d002d1663cc
bhormilind-dev <bhormilind.dev@gmail.com>
2021-11-27 06:25:12 -0500

    #1: Fix lazyLoading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo:'contact', pathMatch: 'full'},
+  { path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
