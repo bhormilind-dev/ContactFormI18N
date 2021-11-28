53541684128134a3ac20903dea82d285d5c6c3a8
bhormilind-dev <bhormilind.dev@gmail.com>
2021-11-28 09:24:32 -0500

    [VIMP]: Temp Change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo:'contact', pathMatch: 'full'},
+  // { path: '', redirectTo:'contact', pathMatch: 'full'},
   { path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)}
 ];
 
